5e11b8eacbf449e246f1e9148a600fff30cca68c
mohsince13 <67540179+mohsince13@users.noreply.github.com>
2020-10-19 21:03:20 +0600

    my

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const fs = require('fs')
 const fileUpload = require('express-fileupload');
 const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i1wb1.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
